07483d1e4614a10ecfc8e2653d86b60dd483ab52
Hongjun Yi <yimunj931@gmail.com>
2023-09-12 09:11:36 +0900

    dd

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+import { config } from "dotenv";
+
+config();
+
+import { IBundler, Bundler } from "@biconomy/bundler";
+import {
+  BiconomySmartAccount,
+  BiconomySmartAccountConfig,
+  DEFAULT_ENTRYPOINT_ADDRESS,
+} from "@biconomy/account";
+import { Wallet, providers, ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
+
+const bundler: IBundler = new Bundler({
+  bundlerUrl: `https://bundler.biconomy.io/api/v2/${ChainId.ARBITRUM_GOERLI_TESTNET}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`,
+  chainId: ChainId.ARBITRUM_GOERLI_TESTNET,
+  entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+});
